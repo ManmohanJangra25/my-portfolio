ffc2b946a1a31f4a7e42ff4fceab2b71b917f101
ManmohanJangra25 <manmohanjangra2528@gmail.com>
2025-01-15 20:38:38 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {
-  createBrowserRouter,
-  Route,
-  Router,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ContentSection from "./components/content/ContentSection";
 import AboutPage from "./components/content/AboutPage";
 import ContactPage from "./components/content/ContactPage";
